f8d87134a3820f7c85f7da6659fa26fc77583b86
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-09 21:11:22 +0300

    ok

--- hello-net/main.go
+++ hello-net/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World net")
+	resp, err := http.Get("www.google.com")
+	if err == nil {
+		fmt.Println(err)
+	}
+	fmt.Println(resp.Body)
 }
