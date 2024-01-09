ed4cd6f84321df97c6c2c64fc6333a80fc6249a4
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-09 21:32:41 +0300

    ok

--- hello-net/main.go
+++ hello-net/main.go
@@ -9,16 +9,19 @@ import (
 
 func main() {
 	fmt.Println("Hello World net")
-	resp, err := http.Get("gopl.io/ch1/fetch")
+	resp, err := http.Get("https://stackoverflow.com/questions/41858635/segmentation-violation-with-golang-channels")
 	if err != nil {
+		fmt.Println("1")
 		os.Exit(1)
 	}
 	b, err := io.ReadAll(resp.Body)
 	resp.Body.Close()
 	if err != nil {
+		fmt.Println("2")
 		os.Exit(1)
 	}
-	for _, data := range b[1:] {
-		fmt.Println(data)
+	for _, data := range b[1:10] {
+
+		fmt.Printf("%b", data)
 	}
 }
