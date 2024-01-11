4b2eed4c8ec48b85e24fffdfb65319bf3cbc3304
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-11 17:25:34 +0300

    ok

--- Reading-database/cmd/app/main.go
+++ Reading-database/cmd/app/main.go
@@ -12,7 +12,10 @@ func main() {
 	db, err := sql.Open("mysql", "yabsera:Deathandlife13579@/app")
 	if err != nil {
 		panic(err)
+	} else if err = db.Ping(); err != nil {
+		panic(err)
 	}
 	// See "Important settings" section.
-	db.Exec("create table user( name varchar(30) password varchar(30))")
+
+	db.Exec("CREATE TABLE IF NOT EXISTS mytable (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, some_text TEXT NOT NULL)")
 }
