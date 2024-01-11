d63c6e30dabcea43883c79d7aed479f5bf5bcf06
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-11 17:15:36 +0300

    ok

--- Reading-database/cmd/app/main.go
+++ Reading-database/cmd/app/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -15,7 +14,5 @@ func main() {
 		panic(err)
 	}
 	// See "Important settings" section.
-	db.SetConnMaxLifetime(time.Minute * 3)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
+	db.Exec("create table user( name varchar(30) password varchar(30))")
 }
