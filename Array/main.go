c9599a9339251a2b6e4a264ca4065973355b531e
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-27 18:39:26 +0300

    ok

--- Array/main.go
+++ Array/main.go
@@ -2,13 +2,15 @@ package main
 
 func main() {
 	num := []int{1, 2, 3}
-	for _, i := range num {
-		println(i)
-	}
+
 	num2 := make([]int, 3)
 
-	for _, i := range num {
-		num2 = append(num2, i*i)
+	for i := 0; i < len(num); i++ {
+		print(i, " ")
 	}
-	println(num2)
+	for _, i := range num2 {
+		println(i)
+	}
+
+	println(len(num2), len(num))
 }
