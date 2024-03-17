5c70925d1298abacc2ea332f5e6711ace158ae24
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-03-17 15:12:55 +0300

    ok

--- readingfromapi/main.go
+++ readingfromapi/main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"io"
+	"log"
+	"os"
 )
 
-func main() {
-
-	url := "https://numbersapi.p.rapidapi.com/1492/year?json=true&fragment=true"
-
-	req, _ := http.NewRequest("GET", url, nil)
-
-	req.Header.Add("X-RapidAPI-Key", "09aedc540emshd7c8ba6f1ecee0ap17ecedjsna99bac53f084")
-	req.Header.Add("X-RapidAPI-Host", "numbersapi.p.rapidapi.com")
-
-	res, _ := http.DefaultClient.Do(req)
-
-	defer res.Body.Close()
-	body, _ := io.ReadAll(res.Body)
-
-	fmt.Println(res)
-	fmt.Println(string(body))
+func main(){
+	file,err:=os.Open("fact.json")
+	if err!=nil{
+		log.Println(err)
+	}
+	defer file.Close()
 
 }
