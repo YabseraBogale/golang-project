aba1944835e5f3e7b28a4da26da6ca928f83449e
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-03-17 15:41:29 +0300

    ok

--- readingfromapi/main.go
+++ readingfromapi/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+
 	"log"
 	"os"
-
 )
 
 func main(){
@@ -14,12 +12,6 @@ func main(){
 		log.Println(err)
 	}
 	defer file.Close()
-	data:=bufio.NewReader(file)
-	d,err:=data.Peek(100)
-	if err!=nil{
-		log.Println(err)
-	}
-	for _,i:=range d{
-		fmt.Print(string(i))
-	}
+
+
 }
