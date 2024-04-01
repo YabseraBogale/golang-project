d81c67a8dbbd2deadad8a56295a35a2788d79d38
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-01 20:09:44 +0300

    ok

--- csv/app.py
+++ csv/app.py
@@ -1,8 +1,19 @@
+from flask import Flask
+
 from requests import get
-from pprint import pprint
 
-url="https://raw.githubusercontent.com/YabseraBogale/cvelistV5/main/cves/2015/0xxx/CVE-2015-0018.json"
 
-data=get(url).json()
+app=Flask(__name__)
 
-pprint(data)
+
+
+
+
+@app.route('')
+def index():
+    url="https://raw.githubusercontent.com/YabseraBogale/cvelistV5/main/cves/2015/0xxx/CVE-2015-0018.json"
+    data=get(url).json()
+    return f"{data}"
+
+if __name__=="__main__":
+    app.run(debug=True)
