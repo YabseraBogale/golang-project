d529a27de03ae62ed1266d5f1cd86a434ee889b8
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-05 18:11:07 +0300

    ok

---
+++ json/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from pprint import pprint
+import pandas as pd
+
+high = pd.read_json("high.json")
+app =Flask(__name__)
+
+#print(high["vulnerabilities"][1999]['cve']['descriptions'][0]['value'])
+pprint(high["vulnerabilities"][1999]['cve'])
+
+
+@app.route("/")
+def index():
+    return high["vulnerabilities"][1999]['cve']
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
