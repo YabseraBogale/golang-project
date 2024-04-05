fcdf579f257be2ffa73479c5f71e94facf5cabbc
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-05 16:30:44 +0300

    made the world

--- gameoflife/main.go
+++ gameoflife/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 )
+
+func worldviwer(grid *[6][6]int) {
+	for i := range grid {
+		for j := range grid {
+			fmt.Print(grid[i][j])
+		}
+		println()
+	}
+}
 
 func main() {
 	grid := [6][6]int{}
 	buildworld(&grid)
+	worldviwer(&grid)
 	census(&grid)
-
+	worldviwer(&grid)
 }
 
 func buildworld(grid *[6][6]int) {
@@ -28,12 +39,18 @@ func census(grid *[6][6]int) {
 		for j := range grid {
 			if i+1 < 6 && j+1 < 6 {
 				sum := grid[i][j] + grid[i][j+1] + grid[i+1][j] + grid[i+1][j+1]
-				if sum == 0 {
-
+				if sum < 2 {
+					grid[i][j] = 0
+					grid[i][j+1] = 0
+					grid[i+1][j] = 0
+					grid[i+1][j+1] = 0
 				} else if sum == 2 || sum == 3 {
 
 				} else if sum == 4 {
-
+					grid[i][j] = 0
+					grid[i][j+1] = 0
+					grid[i+1][j] = 0
+					grid[i+1][j+1] = 0
 				}
 			}
 		}
