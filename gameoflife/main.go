7bdf7f72962c4cac052727abd5f5cb756bae86a6
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-05 16:03:17 +0300

    made the world

--- gameoflife/main.go
+++ gameoflife/main.go
@@ -9,18 +9,19 @@ func main() {
 	grid := [6][6]string{}
 	buildworld(&grid)
 	// census(&grid)
-	fmt.Println(grid)
+	fmt.Println(grid[0][1] == " ")
 }
 
 func buildworld(grid *[6][6]string) {
 	for i := range grid {
 		for j := range grid {
-			if rand.Intn(3) == 0 {
-				grid[i][j] = "0"
-			} else if rand.Intn(3) == 1 {
+			number := rand.Intn(3)
+			if number == 0 {
+				grid[i][j] = " "
+			} else if number == 1 {
 				grid[i][j] = "|"
-			} else if rand.Intn(3) == 2 {
-				grid[i][j] = "__"
+			} else if number == 2 {
+				grid[i][j] = "_"
 			}
 		}
 	}
