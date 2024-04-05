cbe91d7872a7377522502e3c8beca989bb7c1efc
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-05 16:18:59 +0300

    made the world

--- gameoflife/main.go
+++ gameoflife/main.go
@@ -1,33 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 )
 
 func main() {
-	grid := [6][6]string{}
+	grid := [6][6]int{}
 	buildworld(&grid)
-	// census(&grid)
-	fmt.Println(grid[0][1] == " ")
+	census(&grid)
+
 }
 
-func buildworld(grid *[6][6]string) {
+func buildworld(grid *[6][6]int) {
 	for i := range grid {
 		for j := range grid {
-			number := rand.Intn(3)
-			if number == 0 {
-				grid[i][j] = " "
-			} else if number == 1 {
-				grid[i][j] = "|"
-			} else if number == 2 {
-				grid[i][j] = "_"
+			if rand.Intn(2) == 0 {
+				grid[i][j] = 0
+			} else {
+				grid[i][j] = 1
 			}
 		}
 	}
 }
 
-// func census(grid *[6][6]int) {
-// 	grid[0][1] = 100
+func census(grid *[6][6]int) {
+	for i := range grid {
+		for j := range grid {
+			if i+1 < 6 && j+1 < 6 {
+				sum := grid[i][j] + grid[i][j+1] + grid[i+1][j] + grid[i+1][j+1]
+				if sum == 0 {
 
-// }
+				} else if sum == 2 || sum == 3 {
+
+				} else if sum == 4 {
+
+				}
+			}
+		}
+	}
+
+}
