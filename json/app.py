8d835c86fac9a0467689b6550892754c6c58fa4b
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-08 18:12:27 +0300

    ok

--- json/app.py
+++ json/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 high = pd.read_json("high.json")
 critcal=pd.read_json("critcal.json")
+windows=pd.read_json("windows.json")
 app =Flask(__name__)
 
 #print(high["vulnerabilities"][1999]['cve']['descriptions'][0]['value'])
@@ -12,7 +13,7 @@ app =Flask(__name__)
 
 @app.route("/")
 def index():
-    return critcal["vulnerabilities"][1999]['cve']['descriptions'][0]['value']
+    return windows["vulnerabilities"][199]['cve']['descriptions'][0]['value']
 
 
 if __name__=="__main__":
