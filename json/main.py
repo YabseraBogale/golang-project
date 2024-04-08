37cdb136d0001cb0e568347ca66309e17ed7b53c
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-08 18:25:34 +0300

    ok

--- json/main.py
+++ json/main.py
@@ -3,6 +3,10 @@ from pprint import pprint
 import pandas as pd
 
 
+def Data(data):
+    year=data["vulnerabilities"][1999]['cve']['lastModified'].split('T')[0].split('-')[0]
+    vulnerabilities=data["vulnerabilities"][199]['cve']['descriptions'][0]['value']
+    return {"year":year,"vulnerabilities":vulnerabilities}
 high = pd.read_json("high.json")
 
-pprint(high["vulnerabilities"][1999]['cve']['lastModified'].split('T')[0].split('-')[0])
+pprint(high["vulnerabilities"][1999]['cve'])
