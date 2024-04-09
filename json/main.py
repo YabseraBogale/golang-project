ad80912877fa5075722c1cf4e340cb13de7f4cee
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-09 18:02:53 +0300

    ok

--- json/main.py
+++ json/main.py
@@ -6,7 +6,8 @@ import pandas as pd
 def Data(data):
     year=data["vulnerabilities"][1999]['cve']['lastModified'].split('T')[0].split('-')[0]
     vulnerabilities=data["vulnerabilities"][199]['cve']['descriptions'][0]['value']
-    return {"year":year,"vulnerabilities":vulnerabilities}
+    attackComplexity=data["vulnerabilities"][199]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['attackComplexity']
+    return {"year":year,"vulnerabilities":vulnerabilities,'attackComplexity':attackComplexity}
 high = pd.read_json("high.json")
 
-pprint(high["vulnerabilities"][1999]['cve'])
+pprint(high["vulnerabilities"][199]['cve'])
