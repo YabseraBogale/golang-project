ee7644631e0e3578ec5459edbc3283d4c64a4d38
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-10 18:37:56 +0300

    ok

--- json/main.py
+++ json/main.py
@@ -4,35 +4,50 @@ import pandas as pd
 
 import sqlite3
 
-def Data(data):
-    year=data["vulnerabilities"][1999]['cve']['lastModified'].split('T')[0].split('-')[0]
-    vulnerabilities=data["vulnerabilities"][199]['cve']['descriptions'][0]['value']
-    attackComplexity=data["vulnerabilities"][199]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['attackComplexity']
-    baseScore=data["vulnerabilities"][199]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore']
+def Data(name,i):
+      
+    data= pd.read_json(f"{name}.json")
+    year=data["vulnerabilities"][i]['cve']['lastModified'].split('T')[0].split('-')[0]
+    vulnerabilities=data["vulnerabilities"][i]['cve']['descriptions'][0]['value']
+    if str(data["vulnerabilities"][i]['cve']['metrics']).find("cvssMetricV31")<0:
+        attackComplexity=data["vulnerabilities"][i]['cve']['metrics']['cvssMetricV30'][0]['cvssData']['attackComplexity']
+        baseScore=data["vulnerabilities"][i]['cve']['metrics']['cvssMetricV30'][0]['cvssData']['baseScore']
+    else:
+        attackComplexity=data["vulnerabilities"][i]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['attackComplexity']
+        baseScore=data["vulnerabilities"][i]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore']
     return {"year":year,"vulnerabilities":vulnerabilities,'attackComplexity':attackComplexity,"score":baseScore}
-
+   
     
-def makeDabase():
-    statment="""
-        create table if not exists vulnerabilities(
-            id int not null primary key,
-            vulnerability varchar(332) not null,
-            year varchar(4) not null,
-            attackComplexity varchar(10) not null,
-            baseScore varchar(10) not null
-        );
-    """
-    connection=sqlite3.Connection("vulnerabilities.db")
-    pointer=connection.cursor()
-    pointer.execute(statment)
-
-
+def makeDabase(data):
+    try:
+        statment="""
+            create table if not exists vulnerabilities(
+                vulnerability varchar(332) not null,
+                year int not null,
+                attackcomplexity varchar(10) not null,
+                baseScore varchar(10) not null
+            );
+        """
+        connection=sqlite3.Connection("vulnerabilities.db")
+        pointer=connection.cursor()
+        pointer.execute(statment)
+        connection.commit()
+        statment="insert into vulnerabilities(vulnerability,year,attackcomplexity,baseScore) values(?,?,?,?)"
+        pointer.execute(statment,(data["vulnerabilities"],data["year"],data["attackComplexity"],data["score"],))
+        connection.commit()
+        return i
+    except Exception as e:
+        return e
 
 
 ## maxlength of high.json descriptions is 308
 ## maxlength of critcal.json descriptions is 255
 ## maxlength of windows.json descriptions is 332
 
-high= pd.read_json("high.json")
+#high=pd.read_json("high.json")
 
+#print(high["vulnerabilities"][452]['cve']['metrics']['cvssMetricV30'][0]['cvssData']['attackComplexity'])
 
+for i in range(0,2000):
+    data=Data("high",i)
+    print(makeDabase(data))
