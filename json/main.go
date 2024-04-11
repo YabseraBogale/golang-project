439eae96b21084a67fecea990a84f85a17d2ef03
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-11 20:02:55 +0300

    ok

--- json/main.go
+++ json/main.go
@@ -1,5 +1,17 @@
 package main
 
-func mian() {
+import (
+	"database/sql"
+	"fmt"
+	"math/rand"
+	"strconv"
 
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, _ := sql.Open("sqlite3", "data.db")
+	defer db.Close()
+	row, _ := db.Exec("select * Data where id=" + strconv.Itoa(rand.Intn(4000)))
+	fmt.Println(row)
 }
