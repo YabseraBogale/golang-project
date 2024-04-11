2a0405c7e6f33b5beb38a55980d09d9613e88feb
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-11 20:26:16 +0300

    ok

--- json/database.py
+++ json/database.py
@@ -1,20 +1,44 @@
 from random import randint
 import sqlite3
 from pprint import pprint
+"""
+python for 100
 
+real    0m0.078s
+user    0m0.075s
+sys     0m0.004s
+
+golang for 100
+
+real    0m0.467s
+user    0m0.417s
+sys     0m0.140s
+
+
+python for 1000
+
+real    0m0.239s
+user    0m0.195s
+sys     0m0.044s
+
+golang for 1000
+
+real    0m0.866s
+user    0m0.662s
+sys     0m0.250s
+
+"""
 class Database():
-    
     def __init__(self) -> None:
-        self.connection=sqlite3.Connection("vulnerabilities.db")
-        self.pointer=self.connection.cursor()
-    
+        self.connnection=sqlite3.Connection("data.db")
+        self.pointer=self.connnection.cursor()
+
     def fetch(self):
-        statment="select * from vulnerabilities"
+        statment=f"select * from data where id={randint(0,4000)}"
         self.pointer.execute(statment)
-        result=self.pointer.fetchall()
-        return result[randint(0,len(result))]
+        result=self.pointer.fetchone()
+        return result
     
-
-
 test=Database()
-pprint(test.fetch())
+for i in range(0,1000):
+    pprint(test.fetch())
