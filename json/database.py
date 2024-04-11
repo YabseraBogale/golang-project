f23ea77f3b67fb8dcc5807c94b14323f318e41a5
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-11 15:43:10 +0300

    ok

--- json/database.py
+++ json/database.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 class Database():
+    
     def __init__(self) -> None:
         self.connection=sqlite3.Connection("vulnerabilities.db")
         self.pointer=self.connection.cursor()
@@ -9,15 +10,4 @@ class Database():
         statment="select * from vulnerabilities"
         self.pointer.execute(statment)
     
-    def changeType(self):
-        statment="ALTER TABLE table_name MODIFY COLUMN baseScore float;"
-        try:
-            self.pointer.execute(statment)
-            self.connection.commit()
-            return "changed"
-        except Exception as e:
-            return e
-        
-
-test=Database()
-print(test.changeType())
+    
