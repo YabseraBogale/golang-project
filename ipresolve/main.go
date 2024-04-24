88f23f7583380f175a111fc0f6f971e1c97d0063
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-24 15:57:54 +0300

    ok

--- ipresolve/main.go
+++ ipresolve/main.go
@@ -10,11 +10,5 @@ func main() {
 	if err != nil {
 
 	}
-<<<<<<< HEAD
-// how to use ipaddress to find or make my own version of shodan
-	println(ip.String())
-=======
-	println("ip: " + ip.String())
->>>>>>> 6e49f01a11065266d77e11a80637328780ec6ddd
 
 }
