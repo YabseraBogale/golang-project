93f8ae3996ae81c83d9e479b8f8e27fa4d953f08
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-05-26 15:18:52 +0300

    testing it

--- wordcounter/main.go
+++ wordcounter/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"log"
-	"os"
 	"strconv"
 )
 
 func main() {
-	file, err := os.ReadFile("./main.go")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	for _, i := range file {
-		println(strconv.Itoa(i))
-	}
+	// file, err := os.ReadFile("./main.go")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	println(strconv.Atoi("/n"))
 
 }
