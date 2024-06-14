76297fa536c364a4d15075ee784d6efce19ec7df
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-14 22:07:38 +0300

    ok

--- windowrecall/main.go
+++ windowrecall/main.go
@@ -10,22 +10,17 @@ import (
 )
 
 func main() {
-	n := screenshot.NumActiveDisplays()
-	println(n)
-	for i := 0; i < time.Now().Day(); i++ {
-		bounds := screenshot.GetDisplayBounds(1)
-		fmt.Println(bounds)
-		img, err := screenshot.CaptureRect(bounds)
-		if err != nil {
-			panic(err)
-		}
-		timeTaken := time.Now()
-		fileName := fmt.Sprintf("%d-%d-%d_%dx%d.png", timeTaken.Day(), timeTaken.Hour(), timeTaken.Minute(), bounds.Dx(), bounds.Dy())
-		time.Sleep(30 * 1000)
-		file, _ := os.Create(fileName)
-		defer file.Close()
-		png.Encode(file, img)
-		fmt.Printf(bounds.String(), fileName)
 
+	bounds := screenshot.GetDisplayBounds(1)
+	img, err := screenshot.CaptureRect(bounds)
+	if err != nil {
+		panic(err)
 	}
+	timeTaken := time.Now()
+	fileName := fmt.Sprintf("%d_%dx%d.png", timeTaken.Minute(), bounds.Dx(), bounds.Dy())
+	file, _ := os.Create(fileName)
+	defer file.Close()
+	png.Encode(file, img)
+	fmt.Println(bounds.String(), fileName)
+
 }
