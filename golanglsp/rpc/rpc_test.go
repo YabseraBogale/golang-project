44458d2ab81a2f41bb9a872a9a7a8637131a4643
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-15 20:36:58 +0300

    ok

--- golanglsp/rpc/rpc_test.go
+++ golanglsp/rpc/rpc_test.go
@@ -1,1 +1,19 @@
 package rpc_test
+
+import (
+	"testing"
+
+	"github.com/YabseraBogale/golang-project/golanglsp/rpc"
+)
+
+type EncodeTesting struct {
+	Testing bool
+}
+
+func TestEncode(t *testing.T) {
+	expected := "Content-Length: 16\r\n\r\n{\"Testing\":true}"
+	actual := rpc.EncodingMessage(EncodeTesting{Testing: true})
+	if expected != actual {
+		t.Fatalf("Expected: %s Actual: %s", expected, actual)
+	}
+}
