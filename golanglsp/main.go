78c8e800de40c37fe037e366501436ace09dbc75
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-16 18:38:55 +0300

    ok

--- golanglsp/main.go
+++ golanglsp/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bufio"
+	"log"
 	"os"
 
 	"github.com/YabseraBogale/golang-project/golanglsp/rpc"
 )
 
 func main() {
+	logger := getLogger("./logger.txt")
+	logger.Println("Hey i started")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
 	for scanner.Scan() {
@@ -17,3 +20,10 @@ func main() {
 }
 
 func handler(_ any) {}
+func getLogger(filename string) *log.Logger {
+	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("hey you didn't giveme a good file")
+	}
+	return log.New(logfile, "[golanglsp]", log.Ldate|log.LUTC|log.Lshortfile)
+}
