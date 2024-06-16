6996997dbe662ba7fea4411264b4b954f47387bd
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-16 18:57:28 +0300

    ok

---
+++ golanglsp/Readme.md
@@ -0,0 +1,2 @@
+# golanglsp
+golanglsp is an LSP server for Go. The Language Server Protocol allows any text editor to be extended with IDE-like features; see https://langserver.org/ for details. 
--- golanglsp/load_test_lsp.lua
+++ golanglsp/load_test_lsp.lua
@@ -1,3 +1,16 @@
 local client=vim.lsp.start_client {
-    name="golanglsp"
+    name="golanglsp",
+    cmd={"/home/yabsera/Documents/github/golang-project/golanglsp/main"},
 }
+
+if not client then
+    vim.notify "hey, you didn't do the client thingy "
+    return
+end
+
+vim.api.nvim_create_autocmd("filename",{
+    pattern="markdown",
+    callback=function ()
+        vim.lsp.buf_attach_client(0,client)
+    end,
+})
