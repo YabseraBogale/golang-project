79388f99137da60520cd3f9ddb5ffaf225e4d8fd
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-17 15:07:32 +0300

    ok

--- golanglsp/lsp/initialize.go
+++ golanglsp/lsp/initialize.go
@@ -13,3 +13,20 @@ type ClientInfo struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
 }
+
+type InitilizeResponse struct {
+	Response
+	Result InitilizeResult `json:"result"`
+}
+type InitilizeResult struct {
+	Capabilities ServerCapabilities `json:"capabilities"`
+	ServerInfo   ServerInfo         `json:"serverinfo"`
+}
+
+type ServerCapabilities struct {
+}
+
+type ServerInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
