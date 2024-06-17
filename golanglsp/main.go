2b9a2df0010593d4717e847736322a7490688218
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-17 14:41:33 +0300

    ok

--- golanglsp/main.go
+++ golanglsp/main.go
@@ -14,13 +14,18 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
 	for scanner.Scan() {
-		msg := scanner.Text()
-		handler(logger, msg)
+		msg := scanner.Bytes()
+		method, content, err := rpc.DecodingMessage(msg)
+		if err != nil {
+			logger.Println("Got error: %s", err)
+		}
+
+		handler(logger, method, content)
 	}
 }
 
-func handler(logger *log.Logger, msg any) {
-	logger.Println(msg)
+func handler(logger *log.Logger, method string, content []byte) {
+	logger.Println(method)
 }
 func getLogger(filename string) *log.Logger {
 	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
