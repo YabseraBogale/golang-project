702eb9e41ab60e5b9c4b028ec7d768e05362753b
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-27 19:45:54 +0300

    hot reloading not there but much better than expected

--- webapp/main.go
+++ webapp/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	fs := http.ServeFile(http.Dir("./static"))
+	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 	log.Println("listen at 30000")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Println(err.Error())
+	}
 }
