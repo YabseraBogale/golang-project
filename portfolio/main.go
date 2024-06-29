a62ed1578139d4fc899259f9baa6008f5a4a6031
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-29 15:55:14 +0300

    ok

--- portfolio/main.go
+++ portfolio/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 const (
 	port = ":8000"
@@ -8,21 +11,22 @@ const (
 
 func home(writer http.ResponseWriter, request *http.Request) {
 
+	fmt.Fprintln(writer, "home")
 }
 func articles(writer http.ResponseWriter, request *http.Request) {
-
+	fmt.Fprintln(writer, "articles")
 }
 func contact(writer http.ResponseWriter, request *http.Request) {
-
+	fmt.Fprintln(writer, "contact")
 }
 func project(writer http.ResponseWriter, request *http.Request) {
-
+	fmt.Fprintln(writer, "project")
 }
 
 func main() {
 	http.HandleFunc("/", home)
-	http.HandleFunc("/", articles)
-	http.HandleFunc("/", contact)
-	http.HandleFunc("/", project)
+	http.HandleFunc("/articles", articles)
+	http.HandleFunc("/contact", contact)
+	http.HandleFunc("/project", project)
 	http.ListenAndServe(port, nil)
 }
